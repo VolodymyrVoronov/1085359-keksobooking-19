e02eadbf3f02bcd204de34b6ae9e01abd531854d
Volodymyr <malones.vladimir@gmail.com>
2020-01-26 17:28:00 +0100

    adds function createObj, deletes all console.log

--- js/main.js
+++ js/main.js
@@ -5,8 +5,6 @@ blockMap.classList.remove('map--faded');
 
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-// console.log(pinTemplate);
-
 function getRandomItem(array) {
   return Math.floor(Math.random() * array.length);
 }
@@ -15,70 +13,74 @@ function getRandomFromTo(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var AVATAR_OF_AUTHOR = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
-var TITLE = ['Супер предложение', 'Такого вы ещё не видели', 'Лучше, чем сейчас не будет', 'Только попробуй не снять', 'И боги хотели бы тут жить'];
+var AVATAR_OF_AUTHORS = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
+var TITLES = ['Супер предложение', 'Такого вы ещё не видели', 'Лучше, чем сейчас не будет', 'Только попробуй не снять', 'И боги хотели бы тут жить'];
 // var ADDRESS = ['600, 350'];
-var PRICE = [10000, 50000];
-var TYPE = ['palace', 'flat', 'house', 'bungalo'];
+var PRICES = [10000, 50000];
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var AMOUNT_OF_ROOMS = [1, 2, 3];
 var AMOUNT_OF_GUESTS = [1, 2];
-var CHECKIN = ['12:00', '13:00', '14:00'];
-var CHECKOUT = ['12:00', '13:00', '14:00'];
+var CHECKINS = ['12:00', '13:00', '14:00'];
+var CHECKOUTS = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var DESC = ['Лучшее место и место, где свершается обладание и проявляет себя силой помысла, — все это дан пример, т. е. в своей непосредственности проявление видимого органа маги. Особенно показателен тут пример звукового созерцания. Звук — это наиболее сильное из проявлений духовного ощущения...', 'Лучшее место? По уставу это никого не касается. В компьютерную игру дело не идет. Только для психотерапевта обязательно. На второй уровень не пускали. Выпускный класс прошли? Ага. Десять встреч в неделю. И получили испытательный сертификат. Запишите. Про три месяца. Отношение ко мне серьезное.', 'Тот самый отель и сейчас стоял в том же самом номере, из окна которого он тогда смотрел на сидящего за круглым столом худого человечка с трубкой во рту. Вдруг он вспомнил: в последний раз во дворе отеля тот говорил что-то о мудром Петре Великом.'];
+var DESCS = ['Лучшее место и место, где свершается обладание и проявляет себя силой помысла, — все это дан пример, т. е. в своей непосредственности проявление видимого органа маги. Особенно показателен тут пример звукового созерцания. Звук — это наиболее сильное из проявлений духовного ощущения...', 'Лучшее место? По уставу это никого не касается. В компьютерную игру дело не идет. Только для психотерапевта обязательно. На второй уровень не пускали. Выпускный класс прошли? Ага. Десять встреч в неделю. И получили испытательный сертификат. Запишите. Про три месяца. Отношение ко мне серьезное.', 'Тот самый отель и сейчас стоял в том же самом номере, из окна которого он тогда смотрел на сидящего за круглым столом худого человечка с трубкой во рту. Вдруг он вспомнил: в последний раз во дворе отеля тот говорил что-то о мудром Петре Великом.'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-// var PIN_WIDTH = 40;
-// var PIN_HEIGHT = 40;
 
 var OBJECTS_AMOUNT = 8;
 
+var offers = [];
+
 function pullRandomOffer() {
-  var el = {
+  return {
     author: {
-      avatar: AVATAR_OF_AUTHOR[getRandomItem(AVATAR_OF_AUTHOR)],
+      avatar: AVATAR_OF_AUTHORS[getRandomItem(AVATAR_OF_AUTHORS)],
     },
 
     offer: {
-      title: TITLE[getRandomItem(TITLE)],
+      title: TITLES[getRandomItem(TITLES)],
       address: '600, 350',
-      price: PRICE[getRandomItem(PRICE)],
-      type: TYPE[getRandomItem(TYPE)],
+      price: PRICES[getRandomItem(PRICES)],
+      type: TYPES[getRandomItem(TYPES)],
       roomes: AMOUNT_OF_ROOMS[getRandomItem(AMOUNT_OF_ROOMS)],
       guests: AMOUNT_OF_GUESTS[getRandomItem(AMOUNT_OF_GUESTS)],
-      checkin: CHECKIN[getRandomItem(CHECKIN)],
-      checkout: CHECKOUT[getRandomItem(CHECKOUT)],
+      checkin: CHECKINS[getRandomItem(CHECKINS)],
+      checkout: CHECKOUTS[getRandomItem(CHECKOUTS)],
       features: FEATURES[getRandomItem(FEATURES)],
-      discription: DESC[getRandomItem(DESC)],
+      discription: DESCS[getRandomItem(DESCS)],
       photos: PHOTOS[getRandomItem(PHOTOS)],
     },
 
     location: {
-      x: getRandomFromTo(130, 670),
-      y: getRandomFromTo(130, 670),
+      x: getRandomFromTo(40, pinsBlock.clientWidth - 40),
+      y: getRandomFromTo(130, 630),
     },
   };
-  return el;
 }
 
-function renderPin() {
+function createObj(numbers) {
+  for (var i = 0; i < numbers; i++) {
+    offers.push(pullRandomOffer());
+  }
+}
+
+function renderPin(offer) {
   var pinEl = pinTemplate.cloneNode(true);
 
-  pinEl.style = 'left: ' + pullRandomOffer().location.x + 'px; top: ' + pullRandomOffer().location.y + 'px';
-  pinEl.querySelector('img').src = pullRandomOffer().author.avatar;
-  pinEl.querySelector('img').alt = pullRandomOffer().offer.title;
+  pinEl.style = 'left: ' + offer.location.x + 'px; top: ' + offer.location.y + 'px';
+  pinEl.querySelector('img').src = offer.author.avatar;
+  pinEl.querySelector('img').alt = offer.offer.title;
   return pinEl;
 }
 
-// console.log(renderPin());
-
-function renderPins(numbers) {
+function renderPins() {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < numbers; i++) {
-    fragment.appendChild(renderPin());
+  for (var i = 0; i < offers.length; i++) {
+    fragment.appendChild(renderPin(offers[i]));
+
   }
   pinsBlock.appendChild(fragment);
 }
 
-renderPins(OBJECTS_AMOUNT);
+createObj(OBJECTS_AMOUNT);
+renderPins();
 
-// console.log(OBJECTS_AMOUNT);
