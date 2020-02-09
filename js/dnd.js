942c97ec18146ed149bb219134978288eb928c7f
Volodymyr <malones.vladimir@gmail.com>
2020-02-09 13:20:35 +0100

    adds drag and drop functionality

---
+++ js/dnd.js
@@ -0,0 +1,59 @@
+'use strict';
+
+(function () {
+  var pinsBlock = document.querySelector('.map__pins');
+  var mainPin = document.querySelector('.map__pin--main');
+  var inputAddress = document.querySelector('#address');
+
+  var isDragged = false;
+
+  var limits = {
+    top: pinsBlock.offsetTop + 130,
+    right: pinsBlock.offsetWidth + pinsBlock.offsetLeft - mainPin.offsetWidth + 110,
+    bottom: pinsBlock.offsetHeight + pinsBlock.offsetTop - mainPin.offsetHeight + -10,
+    left: pinsBlock.offsetLeft + 110
+  };
+
+  mainPin.addEventListener('mousedown', function (e) {
+    e.preventDefault();
+    isDragged = true;
+    inputAddress.value = (e.clientX + ', ' + e.clientY);
+  });
+
+  pinsBlock.addEventListener('mouseup', function () {
+    isDragged = false;
+  });
+
+  pinsBlock.addEventListener('mousemove', function (e) {
+    e.preventDefault();
+    if (isDragged) {
+      move(e);
+    }
+  });
+
+  function move(e) {
+    var newLocation = {
+      x: limits.left,
+      y: limits.top
+    };
+    if (e.pageX > limits.right) {
+      newLocation.x = limits.right;
+    } else if (e.pageX > limits.left) {
+      newLocation.x = e.pageX;
+    }
+    if (e.pageY > limits.bottom) {
+      newLocation.y = limits.bottom;
+    } else if (e.pageY > limits.top) {
+      newLocation.y = e.pageY;
+    }
+    relocate(newLocation);
+  }
+
+  function relocate(newLocation) {
+    mainPin.style.left = newLocation.x - 110 + 'px';
+    mainPin.style.top = newLocation.y - 30 + 'px';
+    inputAddress.value = (newLocation.x + ', ' + newLocation.y);
+  }
+})();
+
+
