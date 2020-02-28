1608716d8c42d45c8d937d9eb1382bca558f10ea
Volodymyr <malones.vladimir@gmail.com>
2020-02-28 16:33:59 +0100

    adds some changes, fixes some problems with filter, fixes some problems with addEventListeners

--- js/map.js
+++ js/map.js
@@ -2,10 +2,49 @@
 
 (function () {
   var ENTER_KEY = 'Enter';
+  var DEBOUNCE_INTERVAL = 500;
+
   var mainPin = document.querySelector('.map__pin--main');
   var blockMap = document.querySelector('.map');
   var adForm = document.querySelector('.ad-form');
   var inputsOfAdFrom = document.querySelectorAll('.ad-form__element');
+  var filters = document.querySelector('.map__filters');
+  var filterOfTypeOfAccommodation = filters.querySelector('#housing-type');
+  var filterPriceOfAccommodation = filters.querySelector('#housing-price');
+  var filterAmountOfRoomsOfAccommodation = filters.querySelector('#housing-rooms');
+  var filterAmountOfGuestsOfAccommodation = filters.querySelector('#housing-guests');
+  var filterFeaturesOfAccommodation = filters.querySelector('#housing-features').querySelectorAll('input');
+
+  function setValue(proretry) {
+    proretry.value = 'any';
+  }
+
+  function resetAllFilters() {
+    setValue(filterOfTypeOfAccommodation);
+    setValue(filterPriceOfAccommodation);
+    setValue(filterAmountOfRoomsOfAccommodation);
+    setValue(filterAmountOfGuestsOfAccommodation);
+    filterFeaturesOfAccommodation.forEach(function (element) {
+      element.checked = false;
+    });
+  }
+
+  function onFilterChange() {
+    var pinElements = document.querySelectorAll('.map__pin:not(.map__pin--main)');
+    for (var i = 0; i < pinElements.length; i++) {
+      pinElements[i].remove();
+    }
+
+    var lastTimeout;
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(function () {
+      window.updatesFilter();
+    }, DEBOUNCE_INTERVAL);
+
+    window.removeCards();
+  }
 
   function onSubmitBtnClick(evt) {
     window.save(adForm, window.successHandler, window.errorHandler);
@@ -17,6 +56,7 @@
     adForm.classList.remove('ad-form--disabled');
     setEnabled(inputsOfAdFrom);
     adForm.addEventListener('submit', onSubmitBtnClick);
+    filters.addEventListener('change', onFilterChange);
   };
 
   window.deactivateWebsite = function () {
@@ -24,6 +64,8 @@
     adForm.classList.add('ad-form--disabled');
     adForm.removeEventListener('submit', onSubmitBtnClick);
     setDisabled(inputsOfAdFrom);
+    filters.removeEventListener('change', onFilterChange);
+    resetAllFilters();
   };
 
   function setDisabled(array) {
